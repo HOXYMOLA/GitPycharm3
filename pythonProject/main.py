20333f2206de4bec9647c9cc67e43474360ac340
HOXYMOLA <50163360+HOXYMOLA@users.noreply.github.com>
2021-04-11 20:20:35 +0900

    Updates 2 on GitHub Only

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -15,9 +15,12 @@ print("Hello World 4")
 #adding new item in master branch
 print("Hello World 5")
 
-#Updates on mainbranch of Gitgub
+#Updates on mainbranch of GitHub
 print("Hello World Updates on GitHub")
 
 
 #Updates in local master branch
 print("Hello World Updates on Local Master Branch")
+
+#Updates on mainbranch of GitHub
+print("Hello World Updates 2 on GitHub Only")
